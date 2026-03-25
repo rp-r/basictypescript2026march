247f5a53b203288449493c9e6aa972c48367f029
Mohsin Raza <mohsin.raza@fagerstrombolagen.se>
2026-03-25 16:37:40 +0100

    add new file

---
+++ sandbox.js
@@ -0,0 +1,33 @@
+"use strict";
+const character = 'miondd';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(el => {
+    console.log(el);
+});
+let age = 30;
+let isblack = false;
+age = 40;
+console.log(age);
+//We must declare a number 
+const circ = (diameter) => {
+    return diameter * Math.PI;
+};
+console.log(circ(3));
+//OBJECT AND ARRAY
+let names = ['lugi', 'mario', 'yoshi'];
+names.push('toad');
+console.log(names);
+//can not names.push(3) as names is array of string
+let mix = ['anc', 4, 6.7];
+mix.push(5);
+console.log(mix);
+//OBJECTS
+let person = {
+    name: 'mario',
+    age: 30,
+    belt: 'black'
+};
+console.log(person.name);
+//CAN NOT ADD NEW PROPERTY person['study']='Msc';
